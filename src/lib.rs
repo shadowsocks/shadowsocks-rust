e5892855298846e68378239594f0a34261ffbfd3
Y. T. Chung <zonyitoo@gmail.com>
2014-11-03 20:46:03 +0800

    add comments, bump version

--- src/lib.rs
+++ src/lib.rs
@@ -19,18 +19,18 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-/* code */
-
 #![experimental]
-#![crate_type="lib"]
-#![crate_name="shadowsocks"]
+#![license = "MIT"]
+#![crate_type = "lib"]
+#![crate_name = "shadowsocks"]
+#![desc = "Shadowsocks is a fast tunnel proxy that helps you bypass firewalls"]
 #![feature(phase, unsafe_destructor)]
 
 extern crate serialize;
 #[phase(plugin, link)]
 extern crate log;
 
-pub const VERSION: &'static str = "0.9.0";
+pub const VERSION: &'static str = "0.9.1";
 
 pub mod config;
 pub mod relay;
