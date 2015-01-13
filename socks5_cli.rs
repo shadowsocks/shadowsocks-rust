3c0b8f9689bf8c3d7ad84f18b51cfb95c4bfca09
Y. T. Chung <zonyitoo@gmail.com>
2015-01-14 03:15:01 +0800

    updated tester

--- socks5_cli.rs
+++ socks5_cli.rs
@@ -1,8 +1,7 @@
-#![feature(phase)]
-#![feature(globs)]
+#![allow(unstable)]
 
 extern crate getopts;
-#[phase(plugin, link)]
+#[macro_use]
 extern crate log;
 
 extern crate shadowsocks;
@@ -16,14 +15,14 @@ use std::io::net::tcp::TcpStream;
 use std::io::net::ip::SocketAddr;
 use std::io::net::ip::{IpAddr, Port};
 use std::io::net::addrinfo::get_host_addresses;
-use std::io::{MemWriter, BufReader};
+use std::io::BufReader;
 use std::io::stdio::{stdin, stdout};
-use std::io::{Reader, Writer, mod};
+use std::io::{Reader, Writer, self};
 
 use shadowsocks::relay::socks5::*;
 
 fn do_tcp(_: &Matches, svr_addr: &Address, proxy_addr: &SocketAddr) {
-    let mut proxy_stream = TcpStream::connect(proxy_addr.ip.to_string().as_slice(), proxy_addr.port).unwrap();
+    let mut proxy_stream = TcpStream::connect(proxy_addr.to_string().as_slice()).unwrap();
 
     let shake_req = HandshakeRequest::new(vec![0x00]);
     shake_req.write_to(&mut proxy_stream).unwrap();
@@ -35,15 +34,15 @@ fn do_tcp(_: &Matches, svr_addr: &Address, proxy_addr: &SocketAddr) {
 
     let data = stdin().read_to_end().unwrap();
 
-    let req_header = TcpRequestHeader::new(TcpConnect, svr_addr.clone());
+    let req_header = TcpRequestHeader::new(Command::TcpConnect, svr_addr.clone());
     req_header.write_to(&mut proxy_stream).unwrap();
     proxy_stream.write(data.as_slice()).unwrap();
 
     let resp_header = TcpResponseHeader::read_from(&mut proxy_stream).unwrap();
     match resp_header.reply {
-        Succeeded => {},
+        Reply::Succeeded => {},
         _ => {
-            panic!("Failed with error {}", resp_header.reply);
+            panic!("Failed with error {:?}", resp_header.reply);
         }
     }
 
@@ -52,7 +51,7 @@ fn do_tcp(_: &Matches, svr_addr: &Address, proxy_addr: &SocketAddr) {
 
 fn do_udp(matches: &Matches, svr_addr: &Address, proxy_addr: &SocketAddr) {
     let udp_proxy_addr = {
-        let mut proxy_stream = TcpStream::connect(proxy_addr.ip.to_string().as_slice(), proxy_addr.port).unwrap();
+        let mut proxy_stream = TcpStream::connect(proxy_addr.to_string().as_slice()).unwrap();
 
         let shake_req = HandshakeRequest::new(vec![0x00]);
         shake_req.write_to(&mut proxy_stream).unwrap();
@@ -62,14 +61,14 @@ fn do_udp(matches: &Matches, svr_addr: &Address, proxy_addr: &SocketAddr) {
             panic!("Proxy server needs authentication");
         }
 
-        let req_header = TcpRequestHeader::new(UdpAssociate, svr_addr.clone());
+        let req_header = TcpRequestHeader::new(Command::UdpAssociate, svr_addr.clone());
         req_header.write_to(&mut proxy_stream).unwrap();
 
         let resp_header = TcpResponseHeader::read_from(&mut proxy_stream).unwrap();
         match resp_header.reply {
-            Succeeded => {},
+            Reply::Succeeded => {},
             _ => {
-                panic!("Failed with error {}", resp_header.reply);
+                panic!("Failed with error {:?}", resp_header.reply);
             }
         }
 
@@ -77,20 +76,20 @@ fn do_udp(matches: &Matches, svr_addr: &Address, proxy_addr: &SocketAddr) {
     };
 
     let local_addr = SocketAddr {
-        ip: from_str(matches.opt_str("b").expect("Require local address").as_slice()).unwrap(),
-        port: from_str(matches.opt_str("l").expect("Require local port").as_slice()).unwrap(),
+        ip: matches.opt_str("b").expect("Require local address").parse().unwrap(),
+        port: matches.opt_str("l").expect("Require local port").parse().unwrap(),
     };
 
     let mut udp_socket = UdpSocket::bind(local_addr).unwrap();
 
     let proxy_real_addr = match udp_proxy_addr {
-        SocketAddress(ip, port) => SocketAddr {
+        Address::SocketAddress(ip, port) => SocketAddr {
             ip: ip,
             port: port,
         },
-        DomainNameAddress(dm, port) => {
+        Address::DomainNameAddress(dm, port) => {
             SocketAddr {
-                ip: get_host_addresses(dm.as_slice()).unwrap().head().unwrap().clone(),
+                ip: get_host_addresses(dm.as_slice()).unwrap().first().unwrap().clone(),
                 port: port,
             }
         }
@@ -98,14 +97,14 @@ fn do_udp(matches: &Matches, svr_addr: &Address, proxy_addr: &SocketAddr) {
 
     let data = stdin().read_to_end().unwrap();
 
-    let mut bufw = MemWriter::new();
+    let mut bufw = Vec::new();
     let udp_header = UdpAssociateHeader::new(0, svr_addr.clone());
     udp_header.write_to(&mut bufw).unwrap();
     bufw.write(data.as_slice()).unwrap();
-    udp_socket.send_to(bufw.unwrap().as_slice(), proxy_real_addr).unwrap();
+    udp_socket.send_to(bufw.as_slice(), proxy_real_addr).unwrap();
 
-    let mut buf = [0, ..0xffff];
-    let (len, _) = udp_socket.recv_from(buf).unwrap();
+    let mut buf = [0; 0xffff];
+    let (len, _) = udp_socket.recv_from(&mut buf).unwrap();
 
     let mut bufr = BufReader::new(buf.slice_to(len));
     let _ = UdpAssociateHeader::read_from(&mut bufr).unwrap();
@@ -126,11 +125,11 @@ fn main() {
         optopt("t", "protocol", "Protocol to use", "tcp"),
     ];
 
-    let matches = getopts(os::args().tail(), opts).unwrap();
+    let matches = getopts(os::args().tail(), &opts).unwrap();
 
     if matches.opt_present("h") {
         println!("{}", usage(format!("Usage: {} [Options]", os::args()[0]).as_slice(),
-                            opts));
+                             &opts));
         return;
     }
 
@@ -141,14 +140,14 @@ fn main() {
     };
 
     let proxy_addr = SocketAddr {
-        ip: from_str(matches.opt_str("x").expect("Require proxy address").as_slice()).unwrap(),
-        port: from_str(matches.opt_str("o").expect("Require proxy port").as_slice()).unwrap(),
+        ip: matches.opt_str("x").expect("Require proxy address").parse().unwrap(),
+        port: matches.opt_str("o").expect("Require proxy port").parse().unwrap(),
     };
 
-    let svr_port: Port = from_str(matches.opt_str("p").expect("Require server port").as_slice()).unwrap();
-    let svr_addr = match from_str::<IpAddr>(matches.opt_str("s").expect("Require server address").as_slice()) {
-        Some(ip) => SocketAddress(ip, svr_port),
-        None => DomainNameAddress(matches.opt_str("s").unwrap(), svr_port),
+    let svr_port: Port = matches.opt_str("p").expect("Require server port").parse().unwrap();
+    let svr_addr = match matches.opt_str("s").expect("Require server address").parse::<IpAddr>() {
+        Some(ip) => Address::SocketAddress(ip, svr_port),
+        None => Address::DomainNameAddress(matches.opt_str("s").unwrap(), svr_port),
     };
 
     if is_tcp {
