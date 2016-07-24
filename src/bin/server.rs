bde48c6dd2022088b649a29e02b46b8300bd2549
Y. T. Chung <zonyitoo@gmail.com>
2016-07-24 10:20:50 +0800

    adjust stack size for log printing

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -53,58 +53,58 @@ use shadowsocks::relay::{RelayServer, Relay};
 
 fn main() {
     let matches = App::new("shadowsocks")
-                      .version(shadowsocks::VERSION)
-                      .author("Y. T. Chung <zonyitoo@gmail.com>")
-                      .about("A fast tunnel proxy that helps you bypass firewalls.")
-                      .arg(Arg::with_name("VERBOSE")
-                               .short("v")
-                               .multiple(true)
-                               .help("Set the level of debug"))
-                      .arg(Arg::with_name("ENABLE_UDP")
-                               .short("u")
-                               .long("enable-udp")
-                               .help("Enable UDP relay"))
-                      .arg(Arg::with_name("CONFIG")
-                               .short("c")
-                               .long("config")
-                               .takes_value(true)
-                               .help("Specify config file"))
-                      .arg(Arg::with_name("SERVER_ADDR")
-                               .short("s")
-                               .long("server-addr")
-                               .takes_value(true)
-                               .help("Server address"))
-                      .arg(Arg::with_name("SERVER_PORT")
-                               .short("p")
-                               .long("server-port")
-                               .takes_value(true)
-                               .help("Server port"))
-                      .arg(Arg::with_name("LOCAL_ADDR")
-                               .short("b")
-                               .long("local-addr")
-                               .takes_value(true)
-                               .help("Local address, listen only to this address if specified"))
-                      .arg(Arg::with_name("LOCAL_PORT")
-                               .short("l")
-                               .long("local-port")
-                               .takes_value(true)
-                               .help("Local port"))
-                      .arg(Arg::with_name("PASSWORD")
-                               .short("k")
-                               .long("password")
-                               .takes_value(true)
-                               .help("Password"))
-                      .arg(Arg::with_name("ENCRYPT_METHOD")
-                               .short("m")
-                               .long("encrypt-method")
-                               .takes_value(true)
-                               .help("Encryption method"))
-                      .arg(Arg::with_name("THREADS")
-                               .short("t")
-                               .long("threads")
-                               .takes_value(true)
-                               .help("Threads in thread pool"))
-                      .get_matches();
+        .version(shadowsocks::VERSION)
+        .author("Y. T. Chung <zonyitoo@gmail.com>")
+        .about("A fast tunnel proxy that helps you bypass firewalls.")
+        .arg(Arg::with_name("VERBOSE")
+            .short("v")
+            .multiple(true)
+            .help("Set the level of debug"))
+        .arg(Arg::with_name("ENABLE_UDP")
+            .short("u")
+            .long("enable-udp")
+            .help("Enable UDP relay"))
+        .arg(Arg::with_name("CONFIG")
+            .short("c")
+            .long("config")
+            .takes_value(true)
+            .help("Specify config file"))
+        .arg(Arg::with_name("SERVER_ADDR")
+            .short("s")
+            .long("server-addr")
+            .takes_value(true)
+            .help("Server address"))
+        .arg(Arg::with_name("SERVER_PORT")
+            .short("p")
+            .long("server-port")
+            .takes_value(true)
+            .help("Server port"))
+        .arg(Arg::with_name("LOCAL_ADDR")
+            .short("b")
+            .long("local-addr")
+            .takes_value(true)
+            .help("Local address, listen only to this address if specified"))
+        .arg(Arg::with_name("LOCAL_PORT")
+            .short("l")
+            .long("local-port")
+            .takes_value(true)
+            .help("Local port"))
+        .arg(Arg::with_name("PASSWORD")
+            .short("k")
+            .long("password")
+            .takes_value(true)
+            .help("Password"))
+        .arg(Arg::with_name("ENCRYPT_METHOD")
+            .short("m")
+            .long("encrypt-method")
+            .takes_value(true)
+            .help("Encryption method"))
+        .arg(Arg::with_name("THREADS")
+            .short("t")
+            .long("threads")
+            .takes_value(true)
+            .help("Threads in thread pool"))
+        .get_matches();
 
     let mut log_builder = LogBuilder::new();
     log_builder.filter(None, LogLevelFilter::Info);
@@ -138,7 +138,7 @@ fn main() {
                         record.args())
             });
             log_builder.filter(Some("ssserver"), LogLevelFilter::Debug)
-                       .filter(Some("shadowsocks"), LogLevelFilter::Debug);
+                .filter(Some("shadowsocks"), LogLevelFilter::Debug);
         }
         3 => {
             let mut log_builder = log_builder.format(|record: &LogRecord| {
@@ -149,8 +149,8 @@ fn main() {
                         record.args())
             });
             log_builder.filter(Some("sslocal"), LogLevelFilter::Trace)
-                       .filter(Some("shadowsocks"), LogLevelFilter::Trace)
-                       .filter(Some("coio"), LogLevelFilter::Trace);
+                .filter(Some("shadowsocks"), LogLevelFilter::Trace)
+                .filter(Some("coio"), LogLevelFilter::Trace);
         }
         4 => {
             let mut log_builder = log_builder.format(|record: &LogRecord| {
@@ -161,8 +161,8 @@ fn main() {
                         record.args())
             });
             log_builder.filter(Some("sslocal"), LogLevelFilter::Trace)
-                       .filter(Some("shadowsocks"), LogLevelFilter::Trace)
-                       .filter(Some("coio"), LogLevelFilter::Trace);
+                .filter(Some("shadowsocks"), LogLevelFilter::Trace)
+                .filter(Some("coio"), LogLevelFilter::Trace);
         }
         _ => {
             let mut log_builder = log_builder.format(|record: &LogRecord| {
@@ -202,33 +202,21 @@ fn main() {
     let mut has_provided_server_config = false;
 
     if matches.value_of("SERVER_ADDR").is_some() && matches.value_of("SERVER_PORT").is_some() &&
-       matches.value_of("PASSWORD").is_some() &&
-       matches.value_of("ENCRYPT_METHOD").is_some() {
+       matches.value_of("PASSWORD").is_some() && matches.value_of("ENCRYPT_METHOD").is_some() {
         let (svr_addr, svr_port, password, method) = matches.value_of("SERVER_ADDR")
-                                                            .and_then(|svr_addr| {
-                                                                matches.value_of("SERVER_PORT")
-                                                                       .map(|svr_port| {
-                                                                           (svr_addr, svr_port)
-                                                                       })
-                                                            })
-                                                            .and_then(|(svr_addr, svr_port)| {
-                                                                matches.value_of("PASSWORD")
-                                                                       .map(|pwd| {
-                                                                           (svr_addr, svr_port, pwd)
-                                                                       })
-                                                            })
-                                                            .and_then(|(svr_addr,
-                                                                        svr_port,
-                                                                        pwd)| {
-                                                                matches.value_of("ENCRYPT_METHOD")
-                                                                       .map(|m| {
-                                                                           (svr_addr,
-                                                                            svr_port,
-                                                                            pwd,
-                                                                            m)
-                                                                       })
-                                                            })
-                                                            .unwrap();
+            .and_then(|svr_addr| {
+                matches.value_of("SERVER_PORT")
+                    .map(|svr_port| (svr_addr, svr_port))
+            })
+            .and_then(|(svr_addr, svr_port)| {
+                matches.value_of("PASSWORD")
+                    .map(|pwd| (svr_addr, svr_port, pwd))
+            })
+            .and_then(|(svr_addr, svr_port, pwd)| {
+                matches.value_of("ENCRYPT_METHOD")
+                    .map(|m| (svr_addr, svr_port, pwd, m))
+            })
+            .unwrap();
 
         let sc = ServerConfig {
             addr: svr_addr.to_owned(),
@@ -247,8 +235,7 @@ fn main() {
         config.server.push(sc);
         has_provided_server_config = true;
     } else if matches.value_of("SERVER_ADDR").is_none() && matches.value_of("SERVER_PORT").is_none() &&
-       matches.value_of("PASSWORD").is_none() &&
-       matches.value_of("ENCRYPT_METHOD").is_none() {
+       matches.value_of("PASSWORD").is_none() && matches.value_of("ENCRYPT_METHOD").is_none() {
         // Does not provide server config
     } else {
         panic!("`server-addr`, `server-port`, `method` and `password` should be provided together");
@@ -272,21 +259,32 @@ fn main() {
     debug!("Config: {:?}", config);
 
     let threads = matches.value_of("THREADS")
-                         .unwrap_or("1")
-                         .parse::<usize>()
-                         .ok()
-                         .expect("`threads` should be an integer");
+        .unwrap_or("1")
+        .parse::<usize>()
+        .ok()
+        .expect("`threads` should be an integer");
+
+    let stack_size = if matches.occurrences_of("VERBOSE") >= 1 {
+        1 * 1024 * 1024 // 1M stack for formatting!
+    } else {
+        128 * 1024
+    };
+
+    trace!("Coroutine stack size: {}, thread count {}",
+           stack_size,
+           threads);
 
     let show_run_time = matches.occurrences_of("VERBOSE") >= 2;
     Scheduler::new()
         .with_workers(threads)
-        .default_stack_size(128 * 1024)
+        .default_stack_size(stack_size)
         .run(move || {
             if show_run_time {
                 let start_time = PreciseTime::now();
-                Scheduler::spawn(move|| {
+                Scheduler::spawn(move || {
                     loop {
-                        info!("SYSTEM System has already run {}", start_time.to(PreciseTime::now()));
+                        info!("SYSTEM System has already run {}",
+                              start_time.to(PreciseTime::now()));
                         coio::sleep(Duration::from_secs(5));
                     }
                 });
