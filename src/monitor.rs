7e3bc28dc27d235da2d0c71c5412f43e5aba68df
lilydjwg <lilydjwg@gmail.com>
2017-10-08 22:16:21 +0800

    exit normally on SIGTERM

--- src/monitor.rs
+++ src/monitor.rs
@@ -7,31 +7,35 @@ use tokio_core::reactor::Handle;
 
 use futures::{Future, Stream};
 
+use plugin::Plugin;
+
 #[cfg(unix)]
-pub fn monitor_signal(handle: &Handle) {
+pub fn monitor_signal(handle: &Handle, mut plugins: Vec<Plugin>) {
     use tokio_signal::unix::Signal;
 
     let fut = Signal::new(libc::SIGCHLD, handle)
         .and_then(|signal| {
-                      signal.for_each(|_| -> Result<(), io::Error> {
-                                          panic!("Plugin exited unexpectly (SIGCHLD)");
-                                      })
-                  })
+            signal.for_each(|_| -> Result<(), io::Error> {
+                panic!("Plugin exited unexpectly (SIGCHLD)");
+            })
+        })
         .map_err(|err| {
-                     error!("Failed to monitor SIGCHLD, err: {:?}", err);
-                 });
+            error!("Failed to monitor SIGCHLD, err: {:?}", err);
+        });
 
     handle.spawn(fut);
 
     let fut = Signal::new(libc::SIGTERM, handle)
         .and_then(|sigterm| {
-                      sigterm.for_each(|_| -> Result<(), io::Error> {
-                                           panic!("Received SIGTERM, aborting process");
-                                       })
-                  })
+            sigterm.for_each(move |_| -> Result<(), io::Error> {
+                info!("Exiting on SIGTERM");
+                plugins.truncate(0);
+                ::std::process::exit(0);
+            })
+        })
         .map_err(|err| {
-                     error!("Failed to monitor SIGTERM, SIGINT, err: {:?}", err);
-                 });
+            error!("Failed to monitor SIGTERM, err: {:?}", err);
+        });
 
     handle.spawn(fut);
 }
