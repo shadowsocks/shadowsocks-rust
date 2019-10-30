17c21d76afe4c607c93a91f663dadb098858d0d0
zonyitoo <zonyitoo@gmail.com>
2019-10-31 03:59:56 +0800

    Windows signal

--- src/bin/monitor/windows.rs
+++ src/bin/monitor/windows.rs
@@ -1,35 +1,18 @@
-use futures::{self, Future, Stream};
-use log::{error, info};
+use futures::{self, Either};
+use log::info;
 use std::io;
-use tokio::net::signal::windows::Event;
+use tokio::signal::{ctrl_c, windows::ctrl_break};
 
-pub fn create_signal_monitor() -> impl Future<Item = (), Error = io::Error> + Send {
-    // Must be wrapped with a future, because when `Event` is being created, it will spawn
-    // a task into the current executor. So if there is no current running executor, thread
-    // will panic.
-    let events = futures::lazy(|| {
-        Event::ctrl_c()
-            .join(Event::ctrl_break())
-            .and_then(|(c_signals, break_signals)| {
-                // Stream of all signals we care about
-                let signals = c_signals.select(break_signals);
-                // Take only the first signal in the stream and log that it was triggered
-                signals
-                    .take(1)
-                    .for_each(|()| {
-                        info!("Received shutdown signal, exiting");
-                        Ok(())
-                    })
-                    .map_err(|error| {
-                        error!("Error while listening on process signals: {:?}", error);
-                        error
-                    })
-            })
-            .map_err(|error| {
-                error!("Failed to set up process signal monitoring: {:?}", error);
-                error
-            })
-    });
+pub async fn create_signal_monitor() -> io::Result<()> {
+    let cc = ctrl_c()?;
+    let cb = ctrl_break()?;
 
-    events
+    let signal_name = match future::select(cc.into_future(), cb.into_future()).await {
+        Either::Left(..) => "CTRL-C",
+        Either::Right(..) => "CTRL-BREAK",
+    };
+
+    info!("Received {}, exiting", signal_name);
+
+    Ok(())
 }
