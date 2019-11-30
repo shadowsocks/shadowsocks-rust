d310f2f54bfe87a0c77ec5d0bef07415102a1dd1
zonyitoo <zonyitoo@gmail.com>
2019-12-01 02:13:49 +0800

    Revert "Delay doesnt return io::Result"
    This reverts commit 74dc294d165265d20bdcb8c403aa31262f711ab1.

--- src/relay/tcprelay/mod.rs
+++ src/relay/tcprelay/mod.rs
@@ -59,7 +59,7 @@ impl<S> Connection<S> {
         }
     }
 
-    fn poll_read_timeout(&mut self, cx: &mut Context<'_>) -> Poll<()> {
+    fn poll_read_timeout(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         loop {
             if let Some(ref mut timer) = self.read_timer {
                 ready!(Pin::new(timer).poll(cx));
@@ -70,10 +70,10 @@ impl<S> Connection<S> {
                 }
             }
         }
-        Poll::Ready(())
+        Poll::Ready(Ok(()))
     }
 
-    fn poll_write_timeout(&mut self, cx: &mut Context<'_>) -> Poll<()> {
+    fn poll_write_timeout(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         loop {
             if let Some(ref mut timer) = self.write_timer {
                 ready!(Pin::new(timer).poll(cx));
@@ -84,7 +84,7 @@ impl<S> Connection<S> {
                 }
             }
         }
-        Poll::Ready(())
+        Poll::Ready(Ok(()))
     }
 
     fn cancel_read_timeout(&mut self) {
@@ -133,7 +133,7 @@ where
                 Poll::Ready(r)
             }
             Poll::Pending => {
-                ready!(self.poll_read_timeout(cx));
+                ready!(self.poll_read_timeout(cx))?;
                 Poll::Pending
             }
         }
@@ -151,7 +151,7 @@ where
                 Poll::Ready(r)
             }
             Poll::Pending => {
-                ready!(self.poll_write_timeout(cx));
+                ready!(self.poll_write_timeout(cx))?;
                 Poll::Pending
             }
         }
@@ -164,7 +164,7 @@ where
                 Poll::Ready(r)
             }
             Poll::Pending => {
-                ready!(self.poll_write_timeout(cx));
+                ready!(self.poll_write_timeout(cx))?;
                 Poll::Pending
             }
         }
