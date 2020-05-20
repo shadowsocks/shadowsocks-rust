ab80e40dbb11278f9a263a98919cc4236893de5c
Mygod <contact-git@mygod.be>
2020-05-20 19:31:18 -0400

    Warn when using tokio DNS resolver
    Fixes #208.

--- src/relay/dns_resolver/tokio_dns_resolver.rs
+++ src/relay/dns_resolver/tokio_dns_resolver.rs
@@ -1,14 +1,21 @@
 use std::{
     io::{self, Error, ErrorKind},
     net::SocketAddr,
+    sync::atomic::{AtomicBool, Ordering},
 };
 
+use log::warn;
 use tokio::net::lookup_host;
 
 use crate::context::Context;
 
 /// Perform a DNS resolution
 pub async fn resolve(_: &Context, addr: &str, port: u16) -> io::Result<Vec<SocketAddr>> {
+    static TOKIO_USED: AtomicBool = AtomicBool::new(false);
+    if !TOKIO_USED.swap(true, Ordering::Relaxed) {
+        warn!("Tokio resolver is used. Performance might deteriorate.");
+    }
+
     match lookup_host((addr, port)).await {
         Ok(v) => Ok(v.collect()),
         Err(err) => {
