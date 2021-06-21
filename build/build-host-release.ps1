3743ffe1c33f1f854080638262a36ce71dcfd67a
zonyitoo <zonyitoo@gmail.com>
2021-06-21 10:11:55 +0800

    updated build scripts for empty features

--- build/build-host-release.ps1
+++ build/build-host-release.ps1
@@ -1,16 +1,29 @@
+#!pwsh
 <#
     OpenSSL is already installed on windows-latest virtual environment.
     If you need OpenSSL, consider install it by:
 
     choco install openssl
 #>
+param(
+    [Parameter(HelpMessage = "extra features")]
+    [Alias('f')]
+    [string]$Features
+)
+
 $ErrorActionPreference = "Stop"
 
 $TargetTriple = (rustc -Vv | Select-String -Pattern "host: (.*)" | ForEach-Object { $_.Matches.Value }).split()[-1]
 
 Write-Host "Started building release for ${TargetTriple} ..."
 
-cargo build --release
+if ([string]::IsNullOrEmpty($Features)) {
+    cargo build --release
+}
+else {
+    cargo build --release --features "${Features}"
+}
+
 if (!$?) {
     exit $LASTEXITCODE
 }
