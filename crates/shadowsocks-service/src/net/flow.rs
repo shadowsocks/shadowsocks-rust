962377b465ee29c68e687932da35aea2288e4e66
zonyitoo <zonyitoo@gmail.com>
2021-09-07 15:43:14 +0800

    code style optimization with as _

--- crates/shadowsocks-service/src/net/flow.rs
+++ crates/shadowsocks-service/src/net/flow.rs
@@ -29,50 +29,22 @@ impl FlowStat {
     }
 
     /// Transmitted bytes count
-    #[cfg(not(any(target_arch = "mips", target_arch = "powerpc")))]
     pub fn tx(&self) -> u64 {
-        self.tx.load(Ordering::Relaxed)
-    }
-
-    /// Transmitted bytes count
-    #[cfg(any(target_arch = "mips", target_arch = "powerpc"))]
-    pub fn tx(&self) -> u64 {
-        self.tx.load(Ordering::Relaxed) as u64
+        self.tx.load(Ordering::Relaxed) as _
     }
 
     /// Increase transmitted bytes
-    #[cfg(not(any(target_arch = "mips", target_arch = "powerpc")))]
     pub fn incr_tx(&self, n: u64) {
-        self.tx.fetch_add(n, Ordering::AcqRel);
-    }
-
-    /// Increase transmitted bytes
-    #[cfg(any(target_arch = "mips", target_arch = "powerpc"))]
-    pub fn incr_tx(&self, n: u64) {
-        self.tx.fetch_add(n as u32, Ordering::AcqRel);
+        self.tx.fetch_add(n as _, Ordering::AcqRel);
     }
 
     /// Received bytes count
-    #[cfg(not(any(target_arch = "mips", target_arch = "powerpc")))]
     pub fn rx(&self) -> u64 {
-        self.rx.load(Ordering::Relaxed)
-    }
-
-    /// Received bytes count
-    #[cfg(any(target_arch = "mips", target_arch = "powerpc"))]
-    pub fn rx(&self) -> u64 {
-        self.rx.load(Ordering::Relaxed) as u64
+        self.rx.load(Ordering::Relaxed) as _
     }
 
     /// Increase received bytes
-    #[cfg(not(any(target_arch = "mips", target_arch = "powerpc")))]
     pub fn incr_rx(&self, n: u64) {
-        self.rx.fetch_add(n, Ordering::AcqRel);
-    }
-
-    /// Increase received bytes
-    #[cfg(any(target_arch = "mips", target_arch = "powerpc"))]
-    pub fn incr_rx(&self, n: u64) {
-        self.rx.fetch_add(n as u32, Ordering::AcqRel);
+        self.rx.fetch_add(n as _, Ordering::AcqRel);
     }
 }
