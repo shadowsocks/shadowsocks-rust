2bc0bcc321561e7d00635b2818f32f43c49300f8
zonyitoo <zonyitoo@gmail.com>
2022-03-05 00:56:03 +0800

    UDP associations' send channel size 100K
    NOTE: This value is tested with iperf3 locally running 6.40Gbps bitrates
    and keeping lost rate lower than 5.0%.

--- crates/shadowsocks-service/src/local/net/udp/mod.rs
+++ crates/shadowsocks-service/src/local/net/udp/mod.rs
@@ -3,7 +3,7 @@ pub use self::association::{UdpAssociationManager, UdpInboundWrite};
 pub mod association;
 
 /// Packet size for all UDP associations' send queue
-pub const UDP_ASSOCIATION_SEND_CHANNEL_SIZE: usize = 4096;
+pub const UDP_ASSOCIATION_SEND_CHANNEL_SIZE: usize = 102400;
 
 /// Keep-alive channel size for UDP associations' manager
 pub const UDP_ASSOCIATION_KEEP_ALIVE_CHANNEL_SIZE: usize = 256;
