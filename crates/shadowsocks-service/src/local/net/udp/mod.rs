c2c405c340934bcae98e14de3d97f7c62c653f3a
zonyitoo <zonyitoo@gmail.com>
2022-03-05 00:39:02 +0800

    constants should be defined with const

--- crates/shadowsocks-service/src/local/net/udp/mod.rs
+++ crates/shadowsocks-service/src/local/net/udp/mod.rs
@@ -3,7 +3,7 @@ pub use self::association::{UdpAssociationManager, UdpInboundWrite};
 pub mod association;
 
 /// Packet size for all UDP associations' send queue
-pub static UDP_ASSOCIATION_SEND_CHANNEL_SIZE: usize = 8192;
+pub const UDP_ASSOCIATION_SEND_CHANNEL_SIZE: usize = 4096;
 
 /// Keep-alive channel size for UDP associations' manager
-pub static UDP_ASSOCIATION_KEEP_ALIVE_CHANNEL_SIZE: usize = 256;
+pub const UDP_ASSOCIATION_KEEP_ALIVE_CHANNEL_SIZE: usize = 256;
