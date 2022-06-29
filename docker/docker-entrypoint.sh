7ce6945c78ee2b0d71c1b543e9fdc7c3ab756352
vndroid <diracsvip@gmail.com>
2022-06-29 15:02:53 +0800

    Minor update
    - Remove useless configuration.
    - Docker container running as root user, the program running as nobody user

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,28 +10,10 @@ else
 fi
 
 if [ "$1" = "sslocal" -o "$1" = "ssserver" -o "$1" = "ssmanager" -o "$1" = "ssservice" ]; then
-    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
-        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
-
-        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
-        find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
-            case "$f" in
-                *.sh)
-                    if [ -x "$f" ]; then
-                        echo >&3 "$0: Launching $f";
-                        "$f"
-                    else
-                        # warn on shell scripts without exec bit
-                        echo >&3 "$0: Ignoring $f, not executable";
-                    fi
-                    ;;
-                *) echo >&3 "$0: Ignoring $f";;
-            esac
-        done
-
+    if [ -f "/etc/shadowsocks-rust/config.json" ]; then
         echo >&3 "$0: Configuration complete; ready for start up"
     else
-        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+        echo >&3 "$0: No configuration files found in /etc/shadowsocks-rust, skipping configuration"
     fi
 fi
 
