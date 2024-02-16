da9927ce1dd6ef34a75e09fd1e8495b455799d1e
zonyitoo <zonyitoo@gmail.com>
2024-02-16 16:03:32 +0800

    feat: disable ansi if stdout is not terminal

--- src/logging/tracing.rs
+++ src/logging/tracing.rs
@@ -1,5 +1,7 @@
 //! Logging facilities with tracing
 
+use std::io::IsTerminal;
+
 use time::UtcOffset;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{fmt::time::OffsetTime, EnvFilter, FmtSubscriber};
@@ -20,6 +22,13 @@ pub fn init_with_config(bin_name: &str, config: &LogConfig) {
                 OffsetTime::new(UtcOffset::UTC, time::format_description::well_known::Rfc3339)
             }
         });
+
+    // NOTE: ansi is enabled by default.
+    // Could be disabled by `NO_COLOR` environment variable.
+    // https://no-color.org/
+    if !std::io::stdout().is_terminal() {
+        builder = builder.with_ansi(false);
+    }
 
     if debug_level >= 1 {
         builder = builder.with_target(true).with_thread_ids(true).with_thread_names(true);
