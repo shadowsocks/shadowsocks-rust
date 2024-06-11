e981e7eaaf7aafa3226efab3c0df9bde10d555d0
zonyitoo <zonyitoo@gmail.com>
2024-06-11 23:28:08 +0800

    fix: parse_ipnet enabled for local-fake-dns (#1558)

--- src/vparser/mod.rs
+++ src/vparser/mod.rs
@@ -56,7 +56,7 @@ pub fn parse_server_url(v: &str) -> Result<ServerConfig, String> {
     }
 }
 
-#[cfg(feature = "local-tun")]
+#[cfg(any(feature = "local-tun", feature = "local-fake-dns"))]
 pub fn parse_ipnet(v: &str) -> Result<IpNet, String> {
     match v.parse::<IpNet>() {
         Err(..) => Err("should be a CIDR address like 10.1.2.3/24".to_owned()),
